1f9df3ce6458df3cd4eefe3ef5d2996e7b2652e7
MsgIsJmt <tnwls43888@naver.com>
2024-06-07 09:14:43 +0900

    day05 수정

--- day05/js/sub.js
+++ day05/js/sub.js
@@ -1,7 +1,8 @@
 /* 회전버튼 동작 */
 const rotateBtn = document.querySelector('.rotate-btn');
+const autoBtn = document.querySelector('.auto-btn');
 const slides = document.querySelectorAll('.bg-slide');
-const totalSlides = slides.length; // 5
+const totalSlides = slides.length; // 5 
 let index = 0;
 
 // $('.rotate-btn').on('click', function()); 와 동일
@@ -9,20 +10,38 @@ rotateBtn.addEventListener('click', function() {
     rotateBtn.classList.add('active');
     setTimeout(() => {
         rotateBtn.classList.remove('active');
-    }), 2100;
+    }, 2100);
 
     slides.forEach(slide => {
-        if(slide.classList.contains('active')) {
-           slide.classList.add('after-active');
+        if (slide.classList.contains('active')) {
+            slide.classList.add('after-active');
         } else {
-           slide.classList.remove('after-active');
+            slide.classList.remove('after-active');
         }
-    })
+    });
 
     slides[index].classList.remove('active'); // 현재 슬라이드는 active 제거
     index += 1;
 
-    if (index == totalSlides) index = 0; // 슬라이드가 총 슬라이드까지 가면 다시 처음으로 돌아감
+    if (index == totalSlides) index = 0;
+   
+    slides[index].classList.add('active'); // 새 슬라이드에 active 추가
+});
 
-    slides[index].classList.add('active'); // 클릭 시 다음 것을 active를 줘서 화면에 나오게 함
+autoBtn.addEventListener('click', function() {    
+    var i = 0;
+    setInterval(() => {
+        slides.forEach(slide => {
+            if (slide.classList.contains('active')) {
+                slide.classList.add('after-active');
+            } else {
+                slide.classList.remove('after-active');
+            }
+        });
+        
+        slides[i].classList.remove('active');
+        i += 1;
+        if (i == totalSlides) i = 0;
+        slides[i].classList.add('active');
+    }, 5000);
 });
