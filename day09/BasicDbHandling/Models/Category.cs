077800c9faba48bd96cc688ad124a0935586726b
MsgIsJmt <tnwls43888@naver.com>
2024-06-10 17:02:11 +0900

    0610

---
+++ day09/BasicDbHandling/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace BasicDbHandling.Models
+{
+    // DB에 테이블로 만들어지는 엔티티 클래스
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }   
+        public string DisplayOrder { get; set; }
+        public DateTime PostDate { get; set; } = DateTime.Now;    // 기본적으로 생성되는 현재 날짜
+    }
+}
